1b5a74da5aa869f14462e6ed99b41ee89cc2de3a
guojikun <guojikun@y-open.com>
2020-09-30 15:41:49 +0800

    chore：优化prittier配置

--- .prettierrc.js
+++ .prettierrc.js
@@ -2,7 +2,7 @@ module.exports = {
     useTabs: false,
     tabWidth: 4,
     arrowParens: "avoid",
-    printWidth: 120,
+    printWidth: 100,
     semi: true,
     singleQuote: false,
     bracketSpacing: true,
