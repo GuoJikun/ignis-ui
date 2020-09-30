0d35dc7c3bfbcb685df4c0936e2c6044d0b05e21
guojikun <guojikun@y-open.com>
2020-09-30 14:25:09 +0800

    component：添加header，footer，aside，main组件

---
+++ src/components/header/src/header.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="ins-layout-header" :style="getHeight">
+        <slot></slot>
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import { prefix } from "@/utils/assist";
+
+export default defineComponent({
+    name: `${prefix}Header`,
+    props: {
+        height: {
+            type: String,
+            default: "60px",
+        },
+    },
+    computed: {
+        getHeight(): string {
+            return `height:${this.height};`;
+        },
+    },
+});
+</script>
+<style lang="scss">
+.ins-layout-header {
+    box-sizing: border-box;
+    padding: 0 20px;
+    width: 100%;
+    flex-shrink: 0;
+}
+</style>
